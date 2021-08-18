ded306ce1cfd46ea4ccaebb3b33391e2977b9359
LuLu <521wangchen@sjtu.edu.cn>
2021-08-19 00:20:20 +0800

    test Dialog of MUI

--- src/pages/info.jsx
+++ src/pages/info.jsx
@@ -1,5 +1,73 @@
-import { Typography } from '@material-ui/core'
+import {
+  Typography,
+  Dialog,
+  DialogTitle,
+  Button,
+  List,
+  ListItem,
+} from '@material-ui/core'
+import { useState } from 'react'
+
+const SimpleDialog = (props) => {
+  const { onClose, selectedValue, open } = props
+  const handleClose = () => {
+    onClose(selectedValue)
+  }
+
+  const handleListItemClick = (value) => {
+    onClose(value)
+  }
+
+  return (
+    <Dialog
+      onClose={handleClose}
+      open={open}
+    >
+      <DialogTitle>Set back account</DialogTitle>
+      <List>
+        <ListItem button onClick={() => handleListItemClick(1)}>
+          <Typography>1</Typography>
+        </ListItem>
+        <ListItem button onClick={() => handleListItemClick(2)}>
+          <Typography>2</Typography>
+        </ListItem>
+        <ListItem button onClick={() => handleListItemClick(3)}>
+          <Typography>3</Typography>
+        </ListItem>
+      </List>
+    </Dialog>
+  )
+}
 
 export const InfoPage = () => {
-  return <Typography>欢迎来到相关信息页面</Typography>
+  const [open, setOpen] = useState(false)
+  const [selectdValue, setSelectedValue] = useState(-1)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = (value) => {
+    setOpen(false)
+    setSelectedValue(value)
+  }
+  return (
+    <>
+      <Typography>欢迎来到相关信息页面</Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          setOpen(!open)
+        }}
+      >
+        123
+      </Button>
+      <SimpleDialog
+        setSelectedValue={setSelectedValue}
+        open={open}
+        onClose={handleClose}
+      />
+    </>
+  )
 }
